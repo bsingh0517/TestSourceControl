65429afa041e6aa930861385c8d464d1a5c36f73
bsingh0517 <85919103+bsingh0517@users.noreply.github.com>
2024-04-28 04:17:37 -0500

    Added YAY to Message Box

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace TestSourceControl
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string message = "Success! Source Control Added!";
+            string message = "Success! YAY, Source Control Added!";
             MessageBox.Show(message);
         }
     }
